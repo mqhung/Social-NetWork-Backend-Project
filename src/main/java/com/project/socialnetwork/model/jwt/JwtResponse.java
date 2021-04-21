8d63b11f99befd15835af343741a8ee9bf5ccd1b
mrbeonghia <ntnghia199x@gmail.com>
2021-04-21 15:51:02 +0700

    thêm JwtResponse và JwtService

--- src/main/java/com/project/socialnetwork/model/jwt/JwtResponse.java
+++ src/main/java/com/project/socialnetwork/model/jwt/JwtResponse.java
@@ -1,4 +1,15 @@
 package com.project.socialnetwork.model.jwt;
 
+import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
+
+import java.util.Collection;
+
+@Data
 public class JwtResponse {
+    private Long id;
+    private String token;
+    private String type = "Checker";
+    private String username;
+    private Collection<? extends GrantedAuthority> roles;
 }
