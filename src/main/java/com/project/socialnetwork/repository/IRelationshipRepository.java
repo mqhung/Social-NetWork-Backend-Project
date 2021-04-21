1acec06c3d1829d74b2ec6a3d7168a350c9286c0
Hung <mqhung93@gmail.com>
2021-04-21 16:18:07 +0700

    done repo and service relationship

--- src/main/java/com/project/socialnetwork/repository/IRelationshipRepository.java
+++ src/main/java/com/project/socialnetwork/repository/IRelationshipRepository.java
@@ -1,10 +1,14 @@
 package com.project.socialnetwork.repository;
 
 import com.project.socialnetwork.model.Relationship;
+import com.project.socialnetwork.model.RelationshipStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface IRelationshipRepository extends JpaRepository<Relationship, Long> {
-    Iterable<Relationship> findAllBySecondUser(Long id, String status);
+    Iterable<Relationship> findAllBySecondUserIdAndStatus(Long id, RelationshipStatus status);
+    Relationship findRelationshipByFirstUserIdAndSecondUserId(Long firstUserId, Long secondUserId);
+    Iterable<Relationship> findAllByFirstUserIdOrSecondUserIdAndStatus(Long firstUserId, Long secondUserId, RelationshipStatus status);
+    Iterable<Relationship> findAllByFirstUserIdAndStatusOrSecondUserIdAndStatus(Long firstUserId, RelationshipStatus firstStatus, Long secondUserId, RelationshipStatus secondStatus);
 }
