8ea2eb9b1ca6ce163b45e9e3a3ee1cad80bc2124
Hung <mqhung93@gmail.com>
2021-04-22 08:54:40 +0700

    create status repo and service

--- src/main/java/com/project/socialnetwork/controller/RelationshipController.java
+++ src/main/java/com/project/socialnetwork/controller/RelationshipController.java
@@ -1,4 +1,36 @@
 package com.project.socialnetwork.controller;
 
+import com.project.socialnetwork.model.Relationship;
+import com.project.socialnetwork.model.RelationshipStatus;
+import com.project.socialnetwork.service.relationship.IRelationshipService;
+import com.project.socialnetwork.service.status.IStatusService;
+import com.project.socialnetwork.service.user.IUserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/relationship")
 public class RelationshipController {
+    @Autowired
+    private IRelationshipService relationshipService;
+    @Autowired
+    private IStatusService statusService;
+    @Autowired
+    private IUserService userService;
+    @ModelAttribute("listStatus")
+    public Iterable<RelationshipStatus> showAll(){
+        return statusService.findAllStatus();
+    }
+    @GetMapping("/")
+    public ResponseEntity<Iterable<Relationship>> getAll(){
+        return new ResponseEntity<>(relationshipService.findAllRelationship(), HttpStatus.OK);
+    }
+
+
+
 }
