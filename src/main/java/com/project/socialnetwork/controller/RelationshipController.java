40e9c6d72b340e30ca76023f530129adc798a0cc
Dat Bong <Datbong2801@>
2021-04-23 16:13:08 +0700

    update

--- src/main/java/com/project/socialnetwork/controller/RelationshipController.java
+++ src/main/java/com/project/socialnetwork/controller/RelationshipController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
-
+@CrossOrigin("*")
 @RestController
 @RequestMapping("/relationship")
 public class RelationshipController {
