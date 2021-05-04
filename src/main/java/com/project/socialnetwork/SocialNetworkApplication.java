bcdf512ddf61d76e5c256cfb7b6a9d8b5d52fe03
mrbeonghia <ntnghia199x@gmail.com>
2021-05-04 15:23:02 +0700

    sửa lại change password

--- src/main/java/com/project/socialnetwork/SocialNetworkApplication.java
+++ src/main/java/com/project/socialnetwork/SocialNetworkApplication.java
@@ -7,6 +7,7 @@ import com.project.socialnetwork.service.user.IUserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import javax.annotation.PostConstruct;
 import java.util.HashSet;
